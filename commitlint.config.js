fc209d1c72435ee0d2c9830d90e5c9d98ebeb029
jiho.park <jiho.park@yanolja.com>
2022-06-24 16:15:44 +0900

    [XX-00] changed commit lint rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,21 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  plugins: ['commitlint-plugin-function-rules'],
+  rules: {
+    'type-empty': [0],
+    'subject-empty': [0],
+    'type-enum': [0],
+    'function-rules/type-enum': [
+      2,
+      'always',
+      parsed => {
+        const headerRegex = /^\[([A-Z]{2,}-[0-9]+)\] (.*)$/g
+        const isHeaderValid = parsed.header.match(headerRegex)
+        if (isHeaderValid) {
+          return [true]
+        }
+        return [false, `header must match this regex: ${headerRegex}`]
+      }
+    ]
+  }
+}
